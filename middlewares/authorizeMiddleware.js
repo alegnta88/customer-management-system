9144a192ce5179f6b172aa313f5c7f4fd532e70f
alegnta88 <alegnta88@gmail.com>
2026-01-05 18:10:08 +0300

    update middleware

--- middlewares/authorizeMiddleware.js
+++ middlewares/authorizeMiddleware.js
@@ -5,16 +5,16 @@ export const authorizeMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if (!authHeader) {
-      return res.status(401).json({ message: "No token provided" });
+      return res.status(401).json({ message: "No Token Provided Here" });
     }
 
     const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-req.user = { id: decoded.id }; 
-    console.log("Authorized user ID:", req.user.id);
+    req.user = { id: decoded.id };
 
     next();
+    
   } catch (err) {
     return res.status(401).json({ message: "Invalid token" });
   }
